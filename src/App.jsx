88b19227163ec55b5a18fffa23e268e76f3d948a
RingiBoy <mitya.info@gmail.com>
2022-05-01 21:45:13 +0300

    withoutRedux

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+
+  const addTodo = () => {
+    if (text.trim().length) {
+      setTodos([
+        ...todos,
+        { id: new Date().toISOString(), text, completed: false },
+      ]);
+      setText('');
+    }
+  };
+  const toggleTodoComplete = (todoId) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id !== todoId) {
+          return todo;
+        }
+
+        return { ...todo, completed: !todo.completed };
+      })
+    );
+  };
+
+  const removeTodo = (todoId) => {
+    setTodos(todos.filter((todo) => todo.id !== todoId));
+  };
+
+  return (
+    <div className="App">
+      <InputField text={text} handleInput={setText} handleSubmit={addTodo} />
+
+      <TodoList
+        todos={todos}
+        toggleTodoComplete={toggleTodoComplete}
+        removeTodo={removeTodo}
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/InputField.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const InputField = ({text, handleInput, handleSubmit}) => {
+  return (
+    
+    <label>
+        <input value={text} onChange={(e) => handleInput(e.target.value)} />
+        <button onClick={handleSubmit}>AddTodo</button>
+      </label>
+      
+  )
+}
+
+export default InputField
